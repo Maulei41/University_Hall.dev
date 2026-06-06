be1caba2e84146bce5032d1bc3df616590e876df
Maulei <maulei@connect.hku.hk>
2026-06-06 21:46:01 +0800

    adujst mobile header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -36,7 +36,7 @@ export const Header: React.FC = () => {
           {/* Logo */}
           <Link
             to="/"
-            className="flex items-center gap-3 font-display text-4xl font-bold text-brand-gold hover:text-brand-gold-light transition-colors"
+            className="flex items-center gap-3 font-display text-3xl font-bold text-brand-gold hover:text-brand-gold-light transition-colors"
           >
             <img
               src="/University_Hall_Logo.svg"
@@ -44,7 +44,7 @@ export const Header: React.FC = () => {
               className="h-8 sm:h-10 lg:h-16 w-auto"
               style={{ filter: 'brightness(0) invert(1)' }}
             />
-            <span className="inline sm:text-2xl">University Hall</span>
+            <span className="inline ">University Hall</span>
           </Link>
 
           {/* Desktop Navigation */}
